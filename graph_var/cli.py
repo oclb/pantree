da6e2d4ae574ade744773844e7001a136c3a9847
Luke Jen O'Connor <loconnor@broadinstitute.org>
2025-10-14 06:39:24 -0400

    Update cli.py

--- graph_var/cli.py
+++ graph_var/cli.py
@@ -6,44 +6,6 @@ import os
 from .graph import PangenomeGraph
 import click
 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Identify variants in a pangenome graph and write them to a VCF file",
-        formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-    
-    parser.add_argument(
-        "gfa_file",
-        help="Input GFA file path"
-    )
-    
-    parser.add_argument(
-        "vcf_file",
-        help="Output path for VCF file"
-    )
-    
-    parser.add_argument(
-        "--chr-id",
-        help="Chromosome ID for VCF file (default: chr0)",
-        default="chr0"
-    )
-    
-    parser.add_argument(
-        "--ref_name",
-        help="Name of the reference path (default: 'GRCh38')",
-        type=str,
-        default='GRCh38'
-    )
-    
-    parser.add_argument(
-        "--no-genotypes",
-        help="Do not write compute genotypes",
-        action="store_true",
-        default=False
-    )
-    
-    return parser.parse_args()
-
 @click.command()
 @click.argument("gfa_file")
 @click.argument("vcf_file")
