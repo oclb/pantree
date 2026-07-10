a117661cdec8f5bead6241fda0f981227947b71c
Luke Jen O'Connor <loconnor@broadinstitute.org>
2026-07-10 05:41:50 -0400

    Bump version to 0.3.0

--- pantree/__init__.py
+++ pantree/__init__.py
@@ -2,7 +2,7 @@ from .graph import PangenomeGraph
 from .genotype import Genotype
 from .utils import node_complement, edge_complement, sequence_complement, walk_complement
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     'PangenomeGraph',
